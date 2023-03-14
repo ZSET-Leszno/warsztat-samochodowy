2e486ddf4fb203e8db6e385e6ff7c616fe7ddc9f
Jakub Jozefiak <jakubjoz2003@gmail.com>
2023-03-14 13:31:58 +0100

    praca nad panelem, zapis kontrolny (przyciski, sekcja tabele)

--- admin.php
+++ admin.php
@@ -6,62 +6,101 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Warsztat - administrator</title>
     <link rel="stylesheet" href="admin.css">
-    <script src="script.js"></script>
+    <script>
+        function Tabele(){
+            var element = document.getElementById("tabele");
+        if (element.style.display === "block") {
+            element.style.display = "none";
+        } else {
+            element.style.display = "block";
+        }
+        }
+
+        function Raporty(){
+            var element = document.getElementById("raporty");
+        if (element.style.display === "block") {
+            element.style.display = "none";
+        } else {
+            element.style.display = "block";
+        }
+        }
+
+        function Dzialania(){
+            var element = document.getElementById("dzialania");
+        if (element.style.display === "block") {
+            element.style.display = "none";
+        } else {
+            element.style.display = "block";
+        }
+        }
+
+        /*function cheekDisplay(){
+            var element1 = document.getElementById("tabele");
+            var element2 = document.getElementById("raporty");
+            var element3 = document.getElementById("dzialania");
+            if (element1.style.display === "block"){
+                element2.style.display = "none";
+                element3.style.display = "none";
+            }
+            if(element2.style.display === "block")
+            {
+                element1.style.display = "none";
+                element3.style.display = "none";
+            }
+            if(element3.style.display === "block")
+            {
+                element1.style.display = "none";
+                element2.style.display = "none";
+            }
+        }*/
+    </script>
 </head>
 <body>
     <header>
         <a href="index.html"><img src="logo.png" alt="logo"></a>
         <!--MENU-->
         <div id="menu">
-            <a href="/raporty"><button>Raporty </button></a>
-            <a href="/dzialania"><button>Działania</button></a>
             <a href="/"><span>PANEL ADMINISTRATORA</span></a>
         </div>
     </header>
     <main id="content">
-    <script src="https://cdn.jsdelivr.net/npm/page/page.js"></script>
-    <script>
-        // Definicja kontrolerów dla poszczególnych adresów URL
-        page('/', function() {
-            document.getElementById('content').innerHTML = 'To jest strona główna!';
-        });
-        
-        page('/raporty', function() {
-            document.getElementById('content').innerHTML = 'RAPORTY';
-        });
-
-        page('/dzialania', function() {
-            document.getElementById('content').innerHTML = 'DZIALANIA';
-        });
-        
-        // Uruchomienie routera
-        page();
-    </script>
-       <section class="baza">
-        <h3>Tabela - klienci</h3>
-        <table>
-        <?php  
-                $polaczenie = mysqli_connect('localhost', 'root', '', 'warsztat');
-                $klienci = mysqli_query($polaczenie, "SELECT id_klienta, nazwa_firmy, NIP, imie, nazwisko, telefon, email, kod_pocztowy, miejscowosc, adres, samochod FROM klienci;");
-                while($r = mysqli_fetch_row($klienci)){
-                    echo "<tr>";
-                    echo "<td>".$r[0]."</td>";
-                    echo "<td>".$r[1]."</td>";
-                    echo "<td>".$r[2]."</td>";
-                    echo "<td>".$r[3]."</td>";
-                    echo "<td>".$r[4]."</td>";
-                    echo "<td>".$r[5]."</td>";
-                    echo "<td>".$r[6]."</td>";
-                    echo "<td>".$r[7]."</td>";
-                    echo "<td>".$r[8]."</td>";
-                    echo "<td>".$r[9]."</td>";
-                    echo "<td>".$r[10]."</td>";
-                    echo "</tr>";
-                }
-                mysqli_close($polaczenie);
-            ?>
-        </table>
-       </section>
+        <div id="przyciski-kontener">
+            <button class="navi" onclick="Tabele()">
+                Tabele
+            </button>
+            <button class="navi" onclick="Raporty()">
+                Raporty
+            </button>
+            <button class="navi" onclick="Dzialania()">
+                Działania
+            </button>
+        </div>
+    <div id="tabele" class="div">
+            <section class="baza">
+                <h3>Tabela - klienci</h3>
+                <table>
+                <?php  
+                        $polaczenie = mysqli_connect('localhost', 'root', '', 'warsztat');
+                        $klienci = mysqli_query($polaczenie, "SELECT id_klienta, nazwa_firmy, NIP, imie, nazwisko, telefon, email, kod_pocztowy, miejscowosc, adres, samochod FROM klienci;");
+                        while($r = mysqli_fetch_row($klienci)){
+                            echo "<tr>";
+                            echo "<td>".$r[0]."</td>";
+                            echo "<td>".$r[1]."</td>";
+                            echo "<td>".$r[2]."</td>";
+                            echo "<td>".$r[3]."</td>";
+                            echo "<td>".$r[4]."</td>";
+                            echo "<td>".$r[5]."</td>";
+                            echo "<td>".$r[6]."</td>";
+                            echo "<td>".$r[7]."</td>";
+                            echo "<td>".$r[8]."</td>";
+                            echo "<td>".$r[9]."</td>";
+                            echo "<td>".$r[10]."</td>";
+                            echo "</tr>";
+                        }
+                        mysqli_close($polaczenie);
+                    ?>
+            </table>
+        </section>
        <section class="baza">
         <h3>Tabela - marki samochodów</h3>
         <table>
@@ -122,6 +161,20 @@
             ?>
         </table>
        </section>
+    </div>
+
+        <div id="raporty" class="div">
+            <button class="margin">Podgląd klientów</button>
+            <!--<div id="klienci">
+                <button>Osoby prywatne</button>
+                <button>Firmy</button>
+            </div>-->
+            <button class="margin">TOP Raporty</button>
+    </div>
+
+        <div id="dzialania" class="div">
+            TEST 3
+    </div>
     </main>
     <footer>
         <span class="white">STACJA KONTROLI POJAZDÓW
