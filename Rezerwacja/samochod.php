36b2d079328ee5781e91ee9cb3120ddc044fa1f9
Mateusz Klabiski <m.klabinski@zset.leszno.pl>
2023-03-22 15:39:47 +0100

    zmiany

--- Rezerwacja/samochod.php
+++ Rezerwacja/samochod.php
@@ -1,19 +1,9 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
-<html lang="en">
-=======
 <html lang="pl">
->>>>>>> main
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>Document</title>
-</head>
-<body>
-    cos
-=======
     <title>Warsztat - samochod</title>
     <link rel="stylesheet" href="rezerwacja.css">
     <link
@@ -176,6 +166,5 @@
         <br>
         <p>Warsztat Samochodowy Tomasz Zagórski, ul. Augustyna Szamarzewskiego 42, 60-552 Poznań, NIP: 7772507820, REGON: 634640527</p>
     </footer>
->>>>>>> main
 </body>
 </html>
